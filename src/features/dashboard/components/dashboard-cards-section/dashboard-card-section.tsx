782a1a88115c3017f513bd5c39ab68b2ec397cd0
Bernardo <bernardo@Bernardos-MacBook-Pro.local>
2026-04-02 14:19:52 -0300

    feature(): Implement layout card dashboard

--- src/features/dashboard/components/dashboard-cards-section/dashboard-card-section.tsx
+++ src/features/dashboard/components/dashboard-cards-section/dashboard-card-section.tsx
@@ -1,30 +1,71 @@
 import styles from './dashboard-card-section.module.css'
 import {DashboardCard} from "../dashboard-card/dashboard-card.tsx";
 
+type CardContentProps = {
+    title: string
+    value: string
+    subtitle: string
+    percentage: string
+}
+
+const cardsList: CardContentProps[] = [
+    {
+        title: "Total requests",
+        value: "18.4M",
+        subtitle: "vs. período anterior",
+        percentage: "+12.4%",
+    }, {
+        title: "Total requests",
+        value: "18.4M",
+        subtitle: "vs. período anterior",
+        percentage: "+12.4%",
+    }, {
+        title: "Total requests",
+        value: "18.4M",
+        subtitle: "vs. período anterior",
+        percentage: "+12.4%",
+    }, {
+        title: "Total requests",
+        value: "18.4M",
+        subtitle: "vs. período anterior",
+        percentage: "+12.4%",
+    },
+];
+
 export function DashboardCardSection() {
     return (
         <div className={styles.container}>
-            <DashboardCard>
-                <CardContent/>
-            </DashboardCard>
-            <DashboardCard>
-                <CardContent/>
-            </DashboardCard>
+            {cardsList.map((card) => (
+                <DashboardCard>
+                    <CardContent
+                        title={card.title}
+                        value={card.value}
+                        subtitle={card.subtitle}
+                        percentage={card.percentage}
+                    />
+                </DashboardCard>
+            ))}
         </div>
     )
 }
 
-function CardContent() {
+function CardContent(
+    {
+        title,
+        value,
+        subtitle,
+        percentage,
+    }: CardContentProps) {
     return (
-        <div className={styles.container}>
+        <div className={styles.cardContentContainer}>
             <div className={styles.mainInfo}>
-                <h1 className={styles.title}>Total requests</h1>
-                <h1 className={styles.number}>18.4M</h1>
-                <h1 className={styles.subtitle}>vs. período anterior</h1>
+                <h1 className={styles.title}>{title}</h1>
+                <h1 className={styles.number}>{value}</h1>
+                <h1 className={styles.subtitle}>{subtitle}</h1>
             </div>
 
             <div className={styles.percentageContainer}>
-                <h1 className={styles.percentageText}>+12.8%</h1>
+                <h1 className={styles.percentageText}>{percentage}</h1>
             </div>
         </div>
     )
